614f1ebd5d5ed73411bfa87fb1cc40adc8e4a565
Ethan Silvas <ethansilvas2@gmail.com>
2024-05-06 19:06:50 -0700

    init valid palindrome

---
+++ Neetcode/TwoPointers/valid_palindrome.py
@@ -0,0 +1,22 @@
+
+"""
+class Solution(object):
+    def isPalindrome(self, s):
+        converted_s = ""
+        
+        for char in s: 
+            if char.isalnum():
+                converted_s += char.lower()
+
+        i, j = 0, len(converted_s) - 1
+
+        while i < j:
+            if converted_s[i] != converted_s[j]:
+                return False
+
+            i += 1
+            j -= 1
+
+        return True
+
+"""
