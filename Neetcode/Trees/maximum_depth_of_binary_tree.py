f3ad1117e804f626e38b4ba63103e270525c7604
Ethan Silvas <ethansilvas2@gmail.com>
2024-05-08 15:11:02 -0700

    iterative max binary tree

--- Neetcode/Trees/maximum_depth_of_binary_tree.py
+++ Neetcode/Trees/maximum_depth_of_binary_tree.py
@@ -20,4 +20,28 @@ class Solution(object):
         return self.get_max_depth(root, 0)
 
 
+"""
+iterative solution
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        stack = [[root, 1]]
+        max_depth = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                max_depth = max(max_depth, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+   
+
+"""
         
