1011e19eebf204080715b30801c6c6a86eb1169f
Ethan Silvas <ethansilvas2@gmail.com>
2024-05-07 19:05:26 -0700

    update bin search

--- Neetcode/BinarySearch/find_minimum_in_rotated_sorted_array.py
+++ Neetcode/BinarySearch/find_minimum_in_rotated_sorted_array.py
@@ -6,18 +6,16 @@ class Solution(object):
         """
         
         left, right = 0, len(nums) - 1
-        min_num = float("inf")
+        min_element = float('inf')
 
-        while left < right: 
+        while left <= right:
             mid = left + (right - left) // 2
 
-            min_num = min(min_num, nums[mid])
+            min_element = min(min_element, nums[mid])
 
             if nums[mid] > nums[right]:
-                left = mid + 1 
+                left = mid + 1
             else:
                 right = mid - 1
 
-        # check for the case that nums[left] is the min
-        # which if it is then it wouldn't have updated the min
-        return min(min_num, nums[left])
+        return min_element
