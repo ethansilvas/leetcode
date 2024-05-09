ac3c231e3e7304f4f07fc522841fb0483cd5ddf0
Ethan Silvas <ethansilvas2@gmail.com>
2024-05-09 14:21:32 -0700

    valid palindrome

--- Neetcode/TwoPointers/valid_palindrome.py
+++ Neetcode/TwoPointers/valid_palindrome.py
@@ -1,5 +1,31 @@
 class Solution(object):
     def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        left, right = 0, len(s) - 1
+
+        while left <= right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+        
+'''
+class Solution(object):
+    def isPalindrome(self, s):
         converted_s = ""
         
         for char in s: 
@@ -16,3 +42,4 @@ class Solution(object):
             j -= 1
 
         return True
+'''
